10b08c60caa8698c952c2ac187b7da7d88e6131c
Hsiao <Jason.Hsiao@acer.com>
2022-09-05 10:09:52 +0100

    change names to use pep503 normalized names (. become -)

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from jhsiao.namespace import make_ns, fdir
 
 make_ns('jhsiao', dir=fdir(__file__))
 setup(
-    name='jhsiao.ffmpeg',
+    name='jhsiao-ffmpeg',
     version='0.0.1',
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
     description='read from/write to ffmpeg subprocess',
     packages=['jhsiao.ffmpeg'],
     install_requires=[
-        'jhsiao.utils @ git+https://github.com/j-hsiao/py-utils.git',
+        'jhsiao-utils @ git+https://github.com/j-hsiao/py-utils.git',
         'numpy',
         'opencv-python']
 )
